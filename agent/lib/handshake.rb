8cbba33e814b9c0db695685314e636b19191f19c
Chetan Sarva <chetan@pixelcop.net>
2011-02-26 13:57:54 -0500

    use ohai to get mac address

--- agent/lib/handshake.rb
+++ agent/lib/handshake.rb
@@ -1,10 +1,23 @@
 
+require 'ohai'
+
 module Handshake
 
     def register_agent
         url = "http://#{manager_ip}:#{manager_port}/agent/register?ip=#{agent_ip}&uuid=#{agent_uuid}"
         puts url
         puts http_get(url)
+    end
+
+    def get_mac_address
+        o = Ohai::System.new
+        o.require_plugin("os")
+        o.require_plugin("network")
+        addrs = o[:network][:interfaces][ o[:network][:default_interface] ]["addresses"]
+        raise "Unable to find MAC address" if addrs.nil?
+        ret = addrs.find{ |k,v| v["family"] == "lladdr" }
+        raise "Unable to find MAC address" if ret.nil? or ret.empty?
+        ret.first # got it!
     end
 
 end
