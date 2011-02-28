51363e72bed2d81cba14fab9270a882aa47a7fd3
Chetan Sarva <chetan@pixelcop.net>
2011-02-27 20:30:25 -0500

    fixed indent

--- agent/lib/http_client.rb
+++ agent/lib/http_client.rb
@@ -4,22 +4,22 @@ require 'json'
 
 module HttpClient
 
-   def http_get(url)
-       Curl::Easy.http_get(url).body_str
-   end
+    def http_get(url)
+        Curl::Easy.http_get(url).body_str
+    end
 
-   # make an HTTP GET request and parse the JSON response
-   def http_get_json(url)
-       JSON.parse(http_get(url))
-   end
+    # make an HTTP GET request and parse the JSON response
+    def http_get_json(url)
+        JSON.parse(http_get(url))
+    end
 
-   def http_post(url, data)
-       return Curl::Easy.http_post(url, data.map{ |k,v| Curl::PostField.content(k, v) }).body_str
-   end
+    def http_post(url, data)
+        return Curl::Easy.http_post(url, data.map{ |k,v| Curl::PostField.content(k, v) }).body_str
+    end
 
-   # make an HTTP POST request and parse the JSON response
-   def http_post_json(url, data)
-       JSON.parse(http_post(url, data))
-   end
+    # make an HTTP POST request and parse the JSON response
+    def http_post_json(url, data)
+        JSON.parse(http_post(url, data))
+    end
 
 end
