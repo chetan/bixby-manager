b4d0ded929a0173d65228a14797e66efac011538
Chetan Sarva <chetan@pixelcop.net>
2011-03-05 16:14:17 -0500

    fixed mac detection boot; pass agent into server

--- agent/lib/app.rb
+++ agent/lib/app.rb
@@ -5,23 +5,29 @@ class App
 
     include CLI
 
-    def run!
-
+    def load_agent
         uri = @argv.empty? ? nil : @argv.shift
         root_dir = @config[:directory]
 
         agent = Agent.create(uri, root_dir)
+        if not agent.new? and agent.mac_changed? then
+            # loaded from config and mac has changed
+            agent.deregister_agent()
+            agent = Agent.create(uri, root_dir, false)
+        end
         if agent.new? then
-            if agent.mac_changed? then
-                agent.deregister_agent()
-                agent = Agent.create(uri, root_dir, false)
-            end
             agent.register_agent()
             agent.save_config()
         end
+        agent
+    end
 
-        # start the server
+    def run!
+
+        agent = load_agent()
+
         require AGENT_ROOT + "/lib/server"
+        Server.agent = agent
         Server.run!
 
     end
--- agent/lib/server.rb
+++ agent/lib/server.rb
@@ -5,6 +5,14 @@ require File.dirname(__FILE__) + "/rpc"
 
 class Server < Sinatra::Base
 
+    class << self
+        attr_accessor :agent
+    end
+
+    def agent
+        self.class.agent
+    end
+
     get '/op/:operation' do
         operation_name = params[:operation]
 
