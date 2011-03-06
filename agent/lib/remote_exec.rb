f60846cb5add8f1f432318c591791d183f0b7501
Chetan Sarva <chetan@pixelcop.net>
2011-03-05 21:05:45 -0500

    cleanup unused code

--- agent/lib/remote_exec.rb
+++ agent/lib/remote_exec.rb
@@ -24,24 +24,6 @@ module RemoteExec
 
         return run_command(params["command"], params["args"])
 
-
-
-
-
-        operation_name = params[:operation]
-
-        operation = agent.get_operation(operation_name)
-        if operation == nil
-            agent.provision_operation(operation_name)
-            operation = agent.get_operation(operation_name)
-        end
-
-        if operation != nil
-            operation.execute
-        else
-            "Unknown operation #{params[:operation]}"
-        end
-
     end
 
 end
