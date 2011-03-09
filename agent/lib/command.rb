b05f0b1cdcddf6a28b4a9eb56e66645ace919d16
Chetan Sarva <chetan@pixelcop.net>
2011-03-09 00:05:32 -0500

    simple command resolution & execution

---
+++ agent/lib/command.rb
@@ -0,0 +1,47 @@
+
+class Command
+
+    attr_accessor :repo, :package, :command, :args, :env
+
+    # params hash contains:
+    #   repo
+    #   package
+    #   command
+    #   args (optional)
+    #   env (optional)
+    def initialize(params)
+        params.each{ |k,v| self.send("#{k}=", v) }
+    end
+
+    # returns triplet of [ status, stdout, stderr ]
+    def execute
+        status, stdout, stderr = systemu("sh -c '#{self.command_file} #{@args}'")
+    end
+
+    def validate
+        if not package_exists? then
+            raise PackageNotFound.new("repo = #{@repo}; package = #{@package}")
+        end
+
+        if not command_exists? then
+            raise CommandNotFound.new("repo = #{@repo}; package = #{@package}; command = #{@command}")
+        end
+    end
+
+    def package_dir
+        File.join(Agent.agent_root, "repo", @repo, @package)
+    end
+
+    def package_exists?
+        File.exists? self.package_dir
+    end
+
+    def command_file
+        File.join(self.package_dir, @command)
+    end
+
+    def command_exists?
+        File.exists? self.command_file
+    end
+
+end
--- agent/lib/remote_exec.rb
+++ agent/lib/remote_exec.rb
@@ -1,35 +1,22 @@
 
+require File.dirname(__FILE__) + "/command"
 require File.dirname(__FILE__) + "/package_not_found"
+require File.dirname(__FILE__) + "/command_not_found"
 
 require 'systemu'
 
 module RemoteExec
 
-    def package_dir(repo, package)
-        File.join(self.agent_root, "repo", repo, package)
-    end
-
-    def package_exists?(repo, package)
-        File.exists? self.package_dir(repo, package)
-    end
-
-    def run_command(cmd, args, env = nil)
-        status, stdout, stderr = systemu("#{cmd} #{args}")
-    end
-
     # params hash contains:
+    #   repo
     #   package
     #   command
     #   args (optional)
     #   env (optional)
     def exec(params)
-
-        if not package_exists?(params["repo"], params["package"]) then
-            raise PackageNotFound.new("repo = #{params["repo"]}; package = #{params["package"]}")
-        end
-
-        return run_command(params["command"], params["args"])
-
+        cmd = Command.new(params)
+        cmd.validate()
+        return cmd.execute()
     end
 
 end
