17a853321490044a476f837c4d62747ea268c653
Chetan Sarva <chetan@pixelcop.net>
2011-04-20 22:44:48 -0400

    decode JsonResponse inline

--- manager/app/models/agent.rb
+++ manager/app/models/agent.rb
@@ -9,12 +9,10 @@ class Agent < ActiveRecord::Base
 
     include HttpClient
 
+    # execute the given command and return the response
     def run_cmd(cmd)
-
         req = JsonRequest.new("exec", cmd.to_hash)
-        res = http_post_json(agent_uri, req.to_json)
-        # p res
-
+        res = JsonResponse.from_json(http_post_json(agent_uri, req.to_json))
     end
 
     def agent_uri
