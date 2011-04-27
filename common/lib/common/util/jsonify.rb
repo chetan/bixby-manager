2bfcb2e331f5ede1b82609d58ae0519d7e8fd261
Chetan Sarva <chetan@pixelcop.net>
2011-04-26 22:58:00 -0400

    added a dummpy param for to_json to satisfy rails

--- common/lib/common/util/jsonify.rb
+++ common/lib/common/util/jsonify.rb
@@ -2,7 +2,7 @@
 require 'json'
 
 module Jsonify
-    def to_json
+    def to_json(options = nil)
         self.to_json_properties.inject({}) { |h,k| h[k[1,k.length]] = self.instance_eval(k); h }.to_json
     end
     def to_json_properties
