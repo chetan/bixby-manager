3b1cf1cf19e996e05b5228a1d80aa031aa058533
Chetan Sarva <chetan@pixelcop.net>
2011-04-30 20:39:49 -0400

    moved provisioning code into central module

--- agent/repo/system/provisioning/bin/get_bundle.rb
+++ agent/repo/system/provisioning/bin/get_bundle.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
 require File.expand_path(File.join(File.dirname(__FILE__), "/../../../../lib/agent"))
-require AGENT_ROOT + "/server/json_request"
-require AGENT_ROOT + "/server/json_response"
 
-require 'digest'
-require 'fileutils'
+require "server/json_request"
+require "server/json_response"
+require "api/modules/provisioning"
+
+require "digest"
+require "fileutils"
 
 class Provision < BundleCommand
 
@@ -24,35 +26,9 @@ class Provision < BundleCommand
             exit
         end
 
-        files = list_files(cmd)
-        download_files(cmd, files)
+        files = Provisioning.list_files(cmd)
+        Provisioning.download_files(cmd, files)
 
-    end
-
-    def list_files(cmd)
-        req = JsonRequest.new("provisioning:list_files", cmd.to_hash)
-        res = req.exec()
-        return res.data
-    end
-
-    def download_files(cmd, files)
-        local_path = cmd.bundle_dir
-        sha = Digest::SHA1.new
-        p local_path
-        files.each do |f|
-            # see if the file already exists
-            path = File.join(local_path, f['file'])
-            FileUtils.mkdir_p(File.dirname(path))
-            # puts path
-            next if File.file? path and f['sha1'] == sha.hexdigest(File.read(path))
-            # puts "downloading file"
-            req = JsonRequest.new("provisioning:fetch_file", { :cmd => cmd.to_hash, :file => f['file'] })
-            req.exec_download(path)
-            if f['file'] =~ /^bin/ then
-                # correct permissions for executables
-                FileUtils.chmod(0755, path)
-            end
-        end
     end
 
 end
---
+++ common/lib/common/api/modules/provisioning.rb
@@ -0,0 +1,39 @@
+
+require 'api/json_request'
+require 'api/json_response'
+
+require 'api/modules/base_module'
+
+class Provisioning < BaseModule
+
+    class << self
+
+        def list_files(cmd)
+            req = JsonRequest.new("provisioning:list_files", cmd.to_hash)
+            res = req.exec()
+            return res.data
+        end
+
+        def download_files(cmd, files)
+            local_path = cmd.bundle_dir
+            sha = Digest::SHA1.new
+            p local_path
+            files.each do |f|
+                # see if the file already exists
+                path = File.join(local_path, f['file'])
+                FileUtils.mkdir_p(File.dirname(path))
+                # puts path
+                next if File.file? path and f['sha1'] == sha.hexdigest(File.read(path))
+                # puts "downloading file"
+                req = JsonRequest.new("provisioning:fetch_file", { :cmd => cmd.to_hash, :file => f['file'] })
+                req.exec_download(path)
+                if f['file'] =~ /^bin/ then
+                    # correct permissions for executables
+                    FileUtils.chmod(0755, path)
+                end
+            end
+        end
+
+    end # self
+
+end
