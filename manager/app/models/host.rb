b37499285238ad17ddbe79757ab698eda70b82a0
Chetan Sarva <chetan@pixelcop.net>
2011-12-05 13:27:46 -0500

    implemented to_s

--- manager/app/models/host.rb
+++ manager/app/models/host.rb
@@ -4,4 +4,14 @@ class Host < ActiveRecord::Base
   belongs_to :org
   has_one :agent
 
+  def to_s
+    if self.alias() then
+      self.alias()
+    elsif hostname() then
+      hostname()
+    else
+      ip()
+    end
+  end
+
 end
