9ad94ae4b246720312b15b36d711091a70951dcc
Chetan Sarva <chetan@pixelcop.net>
2011-12-08 18:41:58 -0500

    include some basic libraries for plugins

--- agent/lib/agent/model/bundle_command.rb
+++ agent/lib/agent/model/bundle_command.rb
@@ -1,8 +1,19 @@
 
+require "api/json_request"
+require "api/json_response"
+require "api/modules/provisioning"
+require "model/bundle_util"
+
+require "digest"
+require "fileutils"
+
 class BundleCommand
 
-    def initialize
-        @agent = Agent.create
-    end
+  include BundleUtil
+
+  def initialize
+    @agent = Agent.create
+  end
 
 end
+
