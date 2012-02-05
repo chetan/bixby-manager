4e2ffe5f4cd622b67b496ce4e523d07927e6b641
Chetan Sarva <chetan@pixelcop.net>
2012-02-04 19:29:24 -0500

    updated application.rb

--- manager/config/application.rb
+++ manager/config/application.rb
@@ -16,7 +16,7 @@ module Devops
     # config.autoload_paths += %W(#{config.root}/extras)
 
     # add controller subdirs
-    %w(inventory).each do |path|
+    %w(inventory monitoring).each do |path|
       config.autoload_paths << "#{config.root}/app/controllers/#{path}"
     end
 
@@ -49,7 +49,21 @@ module Devops
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    # Enable the new asset pipeline
+    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    # config.active_record.schema_format = :sql
+
+    # Enforce whitelist mode for mass assignment.
+    # This will create an empty whitelist of attributes available for mass-assignment for all models
+    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
+    # parameters by using an attr_accessible or attr_protected declaration.
+    # config.active_record.whitelist_attributes = true
+
+    # Enable the asset pipeline
     config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
