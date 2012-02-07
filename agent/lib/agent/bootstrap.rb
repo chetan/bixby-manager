2090482c62b71ae5d5117fddf3ff2623e6e53748
Chetan Sarva <chetan@pixelcop.net>
2012-02-06 21:19:28 -0500

    cleanup bootstrap process; remove bundler

--- agent/lib/agent/bootstrap.rb
+++ agent/lib/agent/bootstrap.rb
@@ -1,19 +1,6 @@
 
-require 'bundler'
-
-cwd = Dir.pwd
-Dir.chdir(File.dirname(__FILE__))
-Bundler.setup(:default)
-Dir.chdir(cwd)
-
 AGENT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 $:.unshift(AGENT_ROOT) #if not $:.include? AGENT_ROOT
-
-COMMON_ROOT = File.expand_path(File.join(AGENT_ROOT, "../../../common/lib/common"))
-if File.exists? COMMON_ROOT then
-    # means we're running in a dev environment
-    $:.unshift(COMMON_ROOT) if not $:.include? COMMON_ROOT
-end
 
 require 'agent/model/bundle_command'
 
