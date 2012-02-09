ea9e0f602236b20ac44258dcd4d35ec075863e98
Chetan Sarva <chetan@pixelcop.net>
2012-02-09 17:58:40 -0500

    added helper for finding subclasses

--- agent/lib/devops_agent/model/bundle_command.rb
+++ agent/lib/devops_agent/model/bundle_command.rb
@@ -12,5 +12,24 @@ class BundleCommand
     @agent = Agent.create
   end
 
+
+  # retrieve all loaded subclasses of this classs
+  #
+  # @return [Array<Class>] List of subclasses
+  def self.subclasses
+    @subclasses
+  end
+
+
+  private
+
+  def self.inherited(subclass)
+    if superclass.respond_to? :inherited
+      superclass.inherited(subclass)
+    end
+    @subclasses ||= []
+    @subclasses << subclass
+  end
+
 end
 
