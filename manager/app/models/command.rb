04eb1599411413b194cee7c002b71b0716186fe4
Chetan Sarva <chetan@pixelcop.net>
2012-02-11 14:09:44 -0500

    helper for converting to CommandSpec

--- manager/app/models/command.rb
+++ manager/app/models/command.rb
@@ -11,4 +11,10 @@ class Command < ActiveRecord::Base
     File.join(repo.path, bundle, "bin", command)
   end
 
+  def to_command_spec
+    c = CommandSpec.new(self.attributes)
+    c.repo = self.repo.name
+    return c
+  end
+
 end
