cfe1f7f098791d65f1a74465e62a5000cb7d1583
Chetan Sarva <chetan@pixelcop.net>
2012-02-13 14:59:08 -0500

    added test helpers; ease running w/o rake

--- manager/test/test_helper.rb
+++ manager/test/test_helper.rb
@@ -1,6 +1,71 @@
+
+# load simplecov first, doesn't seem to work otherwise
+
+begin
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/test/'
+    add_filter '/config/'
+
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Models', 'app/models'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Libraries', 'lib'
+  end
+  SimpleCov.at_exit do
+    SimpleCov.result.format!
+    # puts SimpleCov.result.filenames
+  end
+rescue Exception => ex
+end
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
+require 'minitest/unit'
+require 'turn'
+require 'turn/reporter'
+require 'turn/reporters/outline_reporter'
+
+Turn.config.framework = :minitest
+Turn.config.format = :outline
+
+module Turn
+  class OutlineReporter < Reporter
+    def start_test(test)
+      @stdout = StringIO.new
+      @stderr = StringIO.new
+
+      name = naturalized_name(test)
+
+      io.print "    %-57s" % name
+
+      @stdout.rewind
+      @stderr.rewind
+
+      $stdout = @stdout
+      $stderr = @stderr unless $DEBUG
+    end
+  end
+end
+
+if $0 == __FILE__ then
+  # require all test cases if not running via rake
+  $: << File.expand_path(File.dirname(__FILE__))
+  Dir.glob(File.expand_path(File.dirname(__FILE__)) + "/**/*.rb").each do |f|
+    next if f =~ /test\/performance/ or f == File.expand_path(__FILE__)
+    require f
+  end
+end
+
+
+# load curb first so webmock can stub it out as necessary
+require 'curb'
+require 'webmock'
+include WebMock::API
+require 'mocha'
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
@@ -11,3 +76,27 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class MiniTest::Unit::TestCase
+
+  # minitest assert_throws doesn't seem to work properly
+  def assert_throws(clazz, msg = nil, &block)
+    begin
+      yield
+    rescue Exception => ex
+      puts "#{ex.class}: #{ex.message}"
+      puts ex.backtrace.join("\n")
+      if clazz.to_s == ex.class.name then
+        if msg.nil?
+          return
+        elsif msg == ex.message then
+          return
+        end
+      end
+    end
+    flunk("Expected #{mu_pp(clazz)} to have been thrown")
+  end
+
+end
+
+MiniTest::Unit.autorun
