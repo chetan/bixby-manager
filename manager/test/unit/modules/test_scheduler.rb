0792da1dfb198fb330d0445269afcf2c53548b7a
Chetan Sarva <chetan@pixelcop.net>
2012-02-17 14:17:38 -0500

    test rest of scheduler methods

--- manager/test/unit/modules/test_scheduler.rb
+++ manager/test/unit/modules/test_scheduler.rb
@@ -3,6 +3,9 @@
 
 class SchedulerTest < ActiveSupport::TestCase
 
+  def setup
+  end
+
   def test_require_class
     require "modules/scheduler"
     assert Object.const_defined? :Scheduler
@@ -13,9 +16,21 @@ class SchedulerTest < ActiveSupport::TestCase
     assert Scheduler.driver == Scheduling::Resque
   end
 
-  def test_schedule_a_job
+  def test_schedule_at
     Resque.reset_delayed_queue
     Scheduler.new.schedule_at((Time.new+30), Scheduling::Job.new("foobar", {}))
+    assert Resque.redis.zcard("delayed_queue_schedule") == 1 # key is namespaced
+  end
+
+  def test_schedule_in
+    Resque.reset_delayed_queue
+    Scheduler.new.schedule_in(30, Scheduling::Job.new("foobar", {}))
+    assert Resque.redis.zcard("delayed_queue_schedule") == 1 # key is namespaced
+  end
+
+  def test_schedule_in_with_queue
+    Resque.reset_delayed_queue
+    Scheduler.new.schedule_in_with_queue(30, Scheduling::Job.new("foobar", {}), "foo")
     assert Resque.redis.zcard("delayed_queue_schedule") == 1 # key is namespaced
   end
 
