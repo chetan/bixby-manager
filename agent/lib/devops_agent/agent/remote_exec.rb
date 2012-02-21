104071f8b6451f1be61d4c0306da80ee42d90712
Chetan Sarva <chetan@pixelcop.net>
2012-02-20 21:14:08 -0500

    validate digest during RemoteExec

--- agent/lib/devops_agent/agent/remote_exec.rb
+++ agent/lib/devops_agent/agent/remote_exec.rb
@@ -13,6 +13,11 @@ module RemoteExec
         @log.debug{ "exec: #{params}" }
         cmd = CommandSpec.new(params)
         cmd.validate()
+
+        digest = params["digest"] || params[:digest]
+        if digest and cmd.digest != digest then
+            raise BundleNotFound, "digest does not match", caller
+        end
         ret = cmd.execute()
         @log.debug{ "ret: " + ret.to_json }
         return ret
--- agent/test/test_agent_exec.rb
+++ agent/test/test_agent_exec.rb
@@ -16,6 +16,12 @@ class TestDevopsAgentExec < MiniTest::Unit::TestCase
     @agent = Agent.create(@manager_uri, @password, @root_dir, @port)
   end
 
+  def setup_root
+    # copy repo to path
+    `mkdir -p #{@root_dir}/repo/support`
+    `cp -a #{@bundle_path} #{@root_dir}/repo/support/`
+  end
+
   def test_exec_error
     # throws the first time
     assert_throws(BundleNotFound) do
@@ -24,9 +30,7 @@ class TestDevopsAgentExec < MiniTest::Unit::TestCase
   end
 
   def test_exec_pass
-    # copy repo to path
-    `mkdir -p #{@root_dir}/repo/support`
-    `cp -a #{@bundle_path} #{@root_dir}/repo/support/`
+    setup_root()
     (status, stdout, stderr) = @agent.exec(@c.to_hash)
     assert status
     assert stdout
@@ -34,6 +38,14 @@ class TestDevopsAgentExec < MiniTest::Unit::TestCase
     assert(status.success?)
     assert_equal("hi\n", stdout)
     assert_equal("", stderr)
+  end
+
+  def test_exec_digest_changed_throws_error
+    setup_root()
+    @c.digest = "lkjasdfasdf"
+    assert_throws(BundleNotFound) do
+      @agent.exec(@c.to_hash)
+    end
   end
 
 end
