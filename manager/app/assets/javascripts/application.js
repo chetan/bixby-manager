cc3a876e5443b9d82c9fc4fd6ca65eec447af583
Chetan Sarva <chetan@pixelcop.net>
2012-03-06 19:37:48 -0500

    split util code out

--- manager/app/assets/javascripts/application.js
+++ manager/app/assets/javascripts/application.js
@@ -2,32 +2,17 @@
 // all vendor assets will be pulled in via this file
 //= require "vendor"
 
+//= require "utils"
 //= require "stark"
 
 // application JS files
 
 //= require "bootstrap"
+//= require "templates"
 
 //= require "main_views"
 
 //= require_tree "./inventory"
 //= require_tree "./monitoring"
 
-//= require "templates"
-
 //= require "start_router"
-
-
-
-
-// TODO cleanup
-
-// $("a.action_btn").click(function() {
-//   $(this).siblings(".dropdown-menu").toggle();
-// });
-
-// $("a.action_btn").popover({ "trigger" : "manual", "content" : });
-//
-// $("a.action_btn").click(function() {
-//   $(this).popover("show");
-// });
