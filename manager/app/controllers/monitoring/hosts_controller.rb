35bd39713ce9f1d23c891b2d65200ec3d70805f6
Chetan Sarva <chetan@pixelcop.net>
2012-04-27 14:34:34 -0400

    use a generic partial for bootstrapping backbone

--- manager/app/controllers/monitoring/hosts_controller.rb
+++ manager/app/controllers/monitoring/hosts_controller.rb
@@ -9,6 +9,11 @@ class Monitoring::HostsController < Monitoring::BaseController
     @host = Host.find(params[:id])
     # TODO error if no id
     @resources = Resource.where(:host_id => @host.id)
+
+    @bootstrap = [
+      { :name => "host", :model => "Host", :data => @host },
+      { :name => "resources", :model => "ResourceList", :data => @resources },
+    ]
   end
 
   def edit
