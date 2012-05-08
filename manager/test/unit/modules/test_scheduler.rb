24db60bd76e8df7266bea188082eed55265848b8
Chetan Sarva <chetan@pixelcop.net>
2012-05-07 20:28:31 -0400

    fixed scheduler test

--- manager/test/unit/modules/test_scheduler.rb
+++ manager/test/unit/modules/test_scheduler.rb
@@ -6,29 +6,29 @@ class SchedulerTest < ActiveSupport::TestCase
 
   def test_require_class
     require "modules/scheduler"
-    assert Object.const_defined? :Scheduler
+    assert Bixby.const_defined? :Scheduler
   end
 
   def test_has_default_driver
-    assert Scheduler.driver
-    assert Scheduler.driver == Scheduling::Resque
+    assert Bixby::Scheduler.driver
+    assert Bixby::Scheduler.driver == Bixby::Scheduler::Resque
   end
 
   def test_schedule_at
     Resque.reset_delayed_queue
-    Scheduler.new.schedule_at((Time.new+30), Scheduling::Job.new("foobar", {}))
+    Bixby::Scheduler.new.schedule_at((Time.new+30), Bixby::Scheduler::Job.new("foobar", {}))
     assert Resque.redis.zcard("delayed_queue_schedule") == 1 # key is namespaced
   end
 
   def test_schedule_in
     Resque.reset_delayed_queue
-    Scheduler.new.schedule_in(30, Scheduling::Job.new("foobar", {}))
+    Bixby::Scheduler.new.schedule_in(30, Bixby::Scheduler::Job.new("foobar", {}))
     assert Resque.redis.zcard("delayed_queue_schedule") == 1 # key is namespaced
   end
 
   def test_schedule_in_with_queue
     Resque.reset_delayed_queue
-    Scheduler.new.schedule_in_with_queue(30, Scheduling::Job.new("foobar", {}), "foo")
+    Bixby::Scheduler.new.schedule_in_with_queue(30, Bixby::Scheduler::Job.new("foobar", {}), "foo")
     assert Resque.redis.zcard("delayed_queue_schedule") == 1 # key is namespaced
   end
 
@@ -41,7 +41,7 @@ class SchedulerTest < ActiveSupport::TestCase
     end
   end
 
-  class FooDriver < Scheduling::Driver
+  class FooDriver < Bixby::Scheduler::Driver
   end
 
 end
