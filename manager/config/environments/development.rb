1f38959d4ae6e887892657a885ed21fa46a3eb87
Chetan Sarva <chetan@pixelcop.net>
2012-05-16 23:39:32 -0400

    disable caching in dev

--- manager/config/environments/development.rb
+++ manager/config/environments/development.rb
@@ -6,6 +6,9 @@ Devops::Application.configure do
   # since you don't have to restart the webserver when you make code changes.
   config.cache_classes = false
 
+  # disable caching in dev
+  config.cache_store = :null_store
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
