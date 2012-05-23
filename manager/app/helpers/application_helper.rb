f4a0b8d9fc94050101547829b4e960a2ba80e550
Chetan Sarva <chetan@pixelcop.net>
2012-05-23 11:02:03 -0400

    remove obsolete helpers

--- manager/app/helpers/application_helper.rb
+++ manager/app/helpers/application_helper.rb
@@ -1,21 +1,5 @@
 module ApplicationHelper
 
-  def navtab(*args)
-
-    if not @current_tab then
-      @current_tab = "Inventory"
-    end
-
-    li = "li"
-    if @current_tab == args.first then
-      li += ".active"
-    end
-
-    haml_tag li do
-      haml_concat link_to(*args)
-    end
-  end
-
 end
 
 # hack to get this to work in haml
--- manager/app/helpers/monitoring_helper.rb
+++ manager/app/helpers/monitoring_helper.rb
@@ -1,26 +1,4 @@
 
 module MonitoringHelper
 
-  # display a form for the given Command's options
-  def form_for_command(cmd)
-    opts = cmd.options
-    opts.keys.each do |key|
-      case opts[key]["type"]
-      when "enum"
-        values = [ "/" ] # TODO load actual opts for enum
-        t = "command[options][#{key}]"
-        html = label_tag(t, key.capitalize + ":")
-        html += select_tag(t, options_from_collection_for_select(values, :to_s, :to_s))
-        return html
-        
-      end
-    end
-  end
-
-  def render_graph(resource)
-    return haml_tag "div.na" do
-      haml_concat "graph not available"
-    end
-  end
-
 end
