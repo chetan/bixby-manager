09305651a9f8fa0de8d2ddab015f9465e9e63690
Chetan Sarva <chetan@pixelcop.net>
2012-05-22 23:18:31 -0400

    split app js into vendor and bixby for easier debugging

--- manager/app/assets/javascripts/application.js
+++ manager/app/assets/javascripts/application.js
@@ -1,18 +1,3 @@
 
 // all vendor assets will be pulled in via this file
 //= require "vendor"
-
-//= require "utils"
-//= require "stark"
-
-// application JS files
-
-//= require "bootstrap"
-//= require "templates"
-
-//= require "main_views"
-
-//= require_tree "./inventory"
-//= require_tree "./monitoring"
-
-//= require "start_router"
---
+++ manager/app/assets/javascripts/bixby.js
@@ -0,0 +1,13 @@
+
+//= require "utils"
+//= require "stark"
+
+//= require "bootstrap"
+//= require "templates"
+
+//= require "main_views"
+
+//= require_tree "./inventory"
+//= require_tree "./monitoring"
+
+//= require "start_router"
