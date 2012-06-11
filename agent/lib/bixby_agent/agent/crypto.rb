99c126fcb191ecce47915f95cd19faf61e463ced
Chetan Sarva <chetan@pixelcop.net>
2012-06-11 19:19:24 -0400

    fixed indentation to two spaces

--- agent/lib/bixby_agent/agent/crypto.rb
+++ agent/lib/bixby_agent/agent/crypto.rb
@@ -7,33 +7,33 @@ class Agent
 
 module Crypto
 
-    # create crypto keypair and save in config folder
-    def create_keypair
-      init_config_dir()
-      pair = EzCrypto::Signer.generate
-      File.open(private_key_file, 'w') { |out| out.write(pair.private_key.to_s) }
-      File.open(public_key_file, 'w') { |out| out.write(pair.public_key.to_s) }
-    end
+  # create crypto keypair and save in config folder
+  def create_keypair
+    init_config_dir()
+    pair = EzCrypto::Signer.generate
+    File.open(private_key_file, 'w') { |out| out.write(pair.private_key.to_s) }
+    File.open(public_key_file, 'w') { |out| out.write(pair.public_key.to_s) }
+  end
 
-    def private_key_file
-      File.join(self.config_dir, "id_rsa")
-    end
+  def private_key_file
+    File.join(self.config_dir, "id_rsa")
+  end
 
-    def public_key_file
-      File.join(self.config_dir, "id_rsa.pub")
-    end
+  def public_key_file
+    File.join(self.config_dir, "id_rsa.pub")
+  end
 
-    def keypair
-      @keypair ||= EzCrypto::Signer.from_file(private_key_file)
-    end
+  def keypair
+    @keypair ||= EzCrypto::Signer.from_file(private_key_file)
+  end
 
-    def public_key
-      self.keypair.public_key.to_s
-    end
+  def public_key
+    self.keypair.public_key.to_s
+  end
 
-    def private_key
-      self.keypair.private_key.to_s
-    end
+  def private_key
+    self.keypair.private_key.to_s
+  end
 
 end # Crypto
 
