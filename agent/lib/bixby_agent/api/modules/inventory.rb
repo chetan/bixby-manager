529e0ca8ed1831c54efa5fb7e7761115a4985c19
Chetan Sarva <chetan@pixelcop.net>
2012-06-22 10:33:24 -0400

    fixed indent to two spaces; cleanup

--- agent/lib/bixby_agent/api/modules/inventory.rb
+++ agent/lib/bixby_agent/api/modules/inventory.rb
@@ -2,15 +2,15 @@
 module Bixby
 class Inventory < BaseModule
 
-    class << self
+  class << self
 
-        def register_agent(uuid, public_key, hostname, port, password)
-            req = JsonRequest.new("inventory:register_agent",
-                    [ uuid, public_key, hostname, port, password ] )
-            return req.exec()
-        end
+    def register_agent(uuid, public_key, hostname, port, password)
+      params = [ uuid, public_key, hostname, port, password ]
+      req = JsonRequest.new("inventory:register_agent", params)
+      return req.exec()
+    end
 
-    end # self
+  end # self
 
 end # Inventory
 end # Bixby
--- agent/lib/bixby_agent/api/modules/provisioning.rb
+++ agent/lib/bixby_agent/api/modules/provisioning.rb
@@ -2,47 +2,47 @@
 module Bixby
 class Provisioning < BaseModule
 
-    class << self
+	class << self
 
-        def list_files(cmd)
-            req = JsonRequest.new("provisioning:list_files", cmd.to_hash)
-            res = req.exec()
-            return res.data
-        end
+		def list_files(cmd)
+			req = JsonRequest.new("provisioning:list_files", cmd.to_hash)
+			res = req.exec()
+			return res.data
+		end
 
-        def download_files(cmd, files)
-            local_path = cmd.bundle_dir
-            digest = cmd.load_digest
-            files.each do |f|
+		def download_files(cmd, files)
+			local_path = cmd.bundle_dir
+			digest = cmd.load_digest
+			files.each do |f|
 
-                fetch = true
-                if not digest then
-                    fetch = true
-                elsif df = digest["files"].find{ |h| h["file"] == f["file"] } then
-                    # compare digest w/ stored one if we have it
-                    fetch = (df["digest"] != f["digest"])
-                else
-                    fetch = true
-                end
+				fetch = true
+				if not digest then
+					fetch = true
+				elsif df = digest["files"].find{ |h| h["file"] == f["file"] } then
+					# compare digest w/ stored one if we have it
+					fetch = (df["digest"] != f["digest"])
+				else
+					fetch = true
+				end
 
-                next if not fetch
+				next if not fetch
 
-                params = cmd.to_hash
-                params.delete(:digest)
+				params = cmd.to_hash
+				params.delete(:digest)
 
-                path = File.join(local_path, f['file'])
-                req = JsonRequest.new("provisioning:fetch_file", [ params, f['file'] ])
-                req.exec_download(path)
-                if f['file'] =~ /^bin/ then
-                    # correct permissions for executables
-                    FileUtils.chmod(0755, path)
-                end
-            end # files.each
+				path = File.join(local_path, f['file'])
+				req = JsonRequest.new("provisioning:fetch_file", [ params, f['file'] ])
+				req.exec_download(path)
+				if f['file'] =~ /^bin/ then
+					# correct permissions for executables
+					FileUtils.chmod(0755, path)
+				end
+			end # files.each
 
-            cmd.update_digest
-        end
+			cmd.update_digest
+		end
 
-    end # self
+	end # self
 
 end # Provisioning
 end # Bixby
