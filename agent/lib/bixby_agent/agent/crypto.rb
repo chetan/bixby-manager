ffcae420a70a95407220293270bcfb2056603165
Chetan Sarva <chetan@pixelcop.net>
2012-06-21 23:46:04 -0400

    removed ezcrypto dependency

--- agent/lib/bixby_agent/agent/crypto.rb
+++ agent/lib/bixby_agent/agent/crypto.rb
@@ -1,7 +1,4 @@
 
-require 'ezcrypto'
-require 'ezsig'
-
 module Bixby
 class Agent
 
@@ -10,21 +7,16 @@ module Crypto
   # create crypto keypair and save in config folder
   def create_keypair
     init_config_dir()
-    pair = EzCrypto::Signer.generate
-    File.open(private_key_file, 'w') { |out| out.write(pair.private_key.to_s) }
-    File.open(public_key_file, 'w') { |out| out.write(pair.public_key.to_s) }
+    pair = OpenSSL::PKey::RSA.generate(2048)
+    File.open(private_key_file, 'w') { |out| out.write(pair.to_s) }
   end
 
   def private_key_file
     File.join(self.config_dir, "id_rsa")
   end
 
-  def public_key_file
-    File.join(self.config_dir, "id_rsa.pub")
-  end
-
   def keypair
-    @keypair ||= EzCrypto::Signer.from_file(private_key_file)
+    @keypair ||= OpenSSL::PKey::RSA.new(File.read(private_key_file))
   end
 
   def public_key
