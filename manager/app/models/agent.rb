e960606fb0d01702d465f9d6b2341a45019905c3
Chetan Sarva <chetan@pixelcop.net>
2012-06-26 14:10:48 -0400

    fixed JsonRequest exec

--- manager/app/models/agent.rb
+++ manager/app/models/agent.rb
@@ -16,7 +16,33 @@ class Agent < ActiveRecord::Base
   # execute the given command and return the response
   def run_cmd(cmd)
     req = Bixby::JsonRequest.new("exec", cmd.to_hash)
-    return req.exec(agent_uri)
+    return exec_api(req)
+  end
+
+  # Execute the given API request
+  #
+  # @param [JsonRequest] json_req
+  # @return [JsonResponse]
+  def exec_api(json_req)
+    begin
+      return Bixby::JsonResponse.from_json(json_req.http_post_json(agent_uri, json_req.to_json))
+    rescue Curl::Err::CurlError => ex
+      return Bixby::JsonResponse.new("fail", ex.message, ex.backtrace)
+    end
+  end
+
+  # Execute the given API download request
+  #
+  # @param [JsonRequest] json_req     Request to download a file
+  # @param [String] download_path     Location to download requested file to
+  # @return [JsonResponse]
+  def exec_api_download(json_req, download_path)
+    begin
+      json_req.http_post_download(agent_uri, json_req.to_json, download_path)
+      return Bixby::JsonResponse.new("success")
+    rescue Curl::Err::CurlError => ex
+      return Bixby::JsonResponse.new("fail", ex.message, ex.backtrace)
+    end
   end
 
   def agent_uri
