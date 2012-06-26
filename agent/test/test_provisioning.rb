eca0bfd8f34e4f2e5d5e2f59890806a669cfb4eb
Chetan Sarva <chetan@pixelcop.net>
2012-06-26 19:56:26 -0400

    fixed test

--- agent/test/test_provisioning.rb
+++ agent/test/test_provisioning.rb
@@ -32,7 +32,8 @@ class TestProvisioning < MiniTest::Unit::TestCase
     Agent.stubs(:create).returns(@agent)
 
     cmd = CommandSpec.new({ :repo => "support", :bundle => "test_bundle", :command => "echo" })
-    ret = Provisioning.list_files(cmd)
+    provisioner = Provision.new
+    ret = provisioner.list_files(cmd)
 
     assert_requested(:post, @manager_uri + "/api", :times => 1) { |req|
       req.body == '{"operation":"provisioning:list_files","params":{"repo":"support","bundle":"test_bundle","command":"echo"}}'
@@ -41,6 +42,8 @@ class TestProvisioning < MiniTest::Unit::TestCase
   end
 
   def test_download_files
+
+    Agent.stubs(:create).returns(@agent)
 
     path = File.expand_path(File.dirname(__FILE__)) + "/support/test_bundle/bin"
     sha = Digest::SHA2.new
@@ -64,7 +67,8 @@ class TestProvisioning < MiniTest::Unit::TestCase
     digest_file = File.join(@root_dir, "repo", "support", "test_bundle", "digest")
     digest_mtime = File::Stat.new(digest_file).mtime.to_i
 
-    Provisioning.download_files(cmd, files)
+    provisioner = Provision.new
+    provisioner.download_files(cmd, files)
 
     assert_requested(req1)
     assert_requested(req2)
