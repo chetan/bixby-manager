4f10db2508ad7bd2ccb1a8516be4da38262b5fb6
Chetan Sarva <chetan@pixelcop.net>
2012-06-27 21:46:53 -0400

    added tests for server

---
+++ agent/test/test_server.rb
@@ -0,0 +1,60 @@
+
+require 'helper'
+require 'rack/test'
+
+module Bixby
+module Test
+
+  class TestServer < TestCase
+
+    include Rack::Test::Methods
+
+    def setup
+      super
+      setup_existing_agent()
+    end
+
+    def app
+      Bixby::Server.agent = @agent
+      Bixby::Server
+    end
+
+    def test_get_is_invalid
+      get "/foobar"
+      assert_equal 200, last_response.status
+      assert last_response.body =~ /invalid request/
+    end
+
+    def test_post_empty_req
+      post "/foobar"
+      assert_equal 200, last_response.status
+      assert last_response.body =~ /invalid request/
+    end
+
+    def test_post_bad_json
+      post "/foobar", "hi there"
+      assert_equal 200, last_response.status
+      assert last_response.body =~ /invalid request/
+    end
+
+    def test_post_invalid_op
+      post "/foobar", JsonRequest.new("hi", {}).to_json
+      assert_equal 200, last_response.status
+      assert last_response.body =~ /unsupported operation/
+    end
+
+    def test_exec
+      `ls`
+      @agent.expects(:exec).once().returns([$?, "", ""])
+      post "/foobar", JsonRequest.new("exec", {}).to_json
+
+      assert_equal 200, last_response.status
+      res = JsonResponse.from_json(last_response.body)
+      assert res.success?
+      assert_equal 0, res.data["status"]
+    end
+
+  end
+
+end
+end
