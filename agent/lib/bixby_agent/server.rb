aeece05973f85843d476d557a62a52d99e7fd727
Chetan Sarva <chetan@pixelcop.net>
2012-07-03 12:45:43 -0400

    implemented agent server request/response encryption

--- agent/lib/bixby_agent/server.rb
+++ agent/lib/bixby_agent/server.rb
@@ -23,13 +23,21 @@ class Server < Sinatra::Base
 
   get '/*' do
     @log.debug { "Disposing of GET request: #{request.path}" }
-    return JsonResponse.invalid_request.to_json
+    return encrypt(JsonResponse.invalid_request.to_json)
   end
 
   post '/*' do
+    return encrypt(handle_request().to_json)
+  end
+
+  def encrypt(json)
+    agent.crypto_enabled? ? agent.encrypt_for_server(json) : json
+  end
+
+  def handle_request
     req = extract_valid_request()
-    if req.kind_of? String then
-      @log.debug { "received a String; returning" }
+    if req.kind_of? JsonResponse then
+      @log.debug { "received a JsonResponse; returning" }
       return req
     end
     @log.debug{ "request: \n#{req.to_json}" }
@@ -43,17 +51,21 @@ class Server < Sinatra::Base
   def extract_valid_request
     body = request.body.read.strip
     if body.nil? or body.empty? then
-      return JsonResponse.invalid_request.to_json
+      return JsonResponse.invalid_request
+    end
+
+    if agent.crypto_enabled? then
+      body = agent.decrypt_from_server(body)
     end
 
     begin
       req = JsonRequest.from_json(body)
     rescue Exception => ex
-      return JsonResponse.invalid_request.to_json
+      return JsonResponse.invalid_request
     end
 
     if not SUPPORTED_OPERATIONS.include? req.operation then
-      return JsonResponse.invalid_request("unsupported operation: #{req.operation}").to_json
+      return JsonResponse.invalid_request("unsupported operation: #{req.operation}")
     end
 
     return req
@@ -67,15 +79,15 @@ class Server < Sinatra::Base
       status, stdout, stderr = agent.exec(req.params)
     rescue Exception => ex
       if ex.kind_of? BundleNotFound then
-        return JsonResponse.bundle_not_found(ex.message).to_json
+        return JsonResponse.bundle_not_found(ex.message)
       elsif ex.kind_of? CommandNotFound then
-        return JsonResponse.command_not_found(ex.message).to_json
+        return JsonResponse.command_not_found(ex.message)
       end
       @log.error(ex)
-      return JsonResponse.new("fail", ex.message, nil, 500).to_json
+      return JsonResponse.new("fail", ex.message, nil, 500)
     end
     data = { :status => status.exitstatus, :stdout => stdout, :stderr => stderr }
-    return JsonResponse.new("success", nil, data).to_json
+    return JsonResponse.new("success", nil, data)
   end
 
 end # Server
--- agent/test/test_server.rb
+++ agent/test/test_server.rb
@@ -44,12 +44,28 @@ module Test
     end
 
     def test_exec
-      `ls`
+      `ls` # hack to create a Process::Status object we can pass to our stub
       @agent.expects(:exec).once().returns([$?, "", ""])
       post "/foobar", JsonRequest.new("exec", {}).to_json
 
       assert_equal 200, last_response.status
       res = JsonResponse.from_json(last_response.body)
+      assert res.success?
+      assert_equal 0, res.data["status"]
+    end
+
+    def test_exec_encrypted
+      ENV["BIXBY_NOCRYPTO"] = "0"
+      `ls` # hack to create a Process::Status object we can pass to our stub
+      @agent.expects(:exec).once().returns([$?, "", ""])
+      post "/foobar", Base64.encode64(@agent.private_key.public_encrypt(JsonRequest.new("exec", {}).to_json))
+
+      assert_equal 200, last_response.status
+
+      server_key = OpenSSL::PKey::RSA.new(File.read(File.join(@root_dir, "etc", "server")))
+      json = server_key.private_decrypt(Base64.decode64(last_response.body))
+
+      res = JsonResponse.from_json(json)
       assert res.success?
       assert_equal 0, res.data["status"]
     end
