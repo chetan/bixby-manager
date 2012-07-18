afbc2297f686d46dd3697ac86f2b5cfab37d17f6
Chetan Sarva <chetan@pixelcop.net>
2012-07-17 22:42:31 -0400

    refactored the ApiController

--- manager/app/controllers/api_controller.rb
+++ manager/app/controllers/api_controller.rb
@@ -9,12 +9,40 @@ class ApiController < ApplicationController
 
   def handle
 
+    begin
+
+      ret = handle_request()
+
+      # return response
+      if ret.kind_of? Bixby::FileDownload then
+        return send_file(ret.filename, :filename => File.basename(ret.filename))
+
+      elsif ret.kind_of? Bixby::JsonResponse then
+        return render(:json => ret.to_json)
+      end
+
+      return render(:json => Bixby::JsonResponse.new(:success, nil, ret).to_json)
+
+    rescue Exception => ex
+      puts ex
+      puts ex.backtrace
+      return render(:json => Bixby::JsonResponse.new(:fail, ex.message, ex, 500).to_json)
+    end
+
+  end # handle
+
+
+  private
+
+  # Handle the API request
+  #
+  # @return [JsonResponse]
+  def handle_request
+
     # extract JsonRequest
 
     req = extract_request()
-    if req.kind_of? Bixby::JsonResponse then
-      return render(:json => req.to_json)
-    end
+    return req if req.kind_of? Bixby::JsonResponse
 
 
     # validate request of form: operation = "module_name:method_name"
@@ -42,41 +70,29 @@ class ApiController < ApplicationController
 
     # execute request
 
-    begin
-      # req = http request object
-      if req.params.kind_of? Hash then
-        ret = mod.send(op, HashWithIndifferentAccess.new(req.params))
-      elsif req.params.kind_of? Array then
-        ret = mod.send(op, *req.params)
-      else
-        ret = mod.send(op, req.params)
-      end
-
-      # return response
-      if ret.kind_of? Bixby::FileDownload then
-        return send_file(ret.filename, :filename => File.basename(ret.filename))
-
-      elsif ret.kind_of? Bixby::JsonResponse then
-        return render(:json => ret.to_json)
-      end
-
-      return render(:json => Bixby::JsonResponse.new(:success, nil, ret).to_json)
-
-    rescue Exception => ex
-      puts ex
-      puts ex.backtrace
-      return render(:json => Bixby::JsonResponse.new(:fail, ex.message, ex, 500).to_json)
+    # req = http request object
+    if req.params.kind_of? Hash then
+      return mod.send(op, HashWithIndifferentAccess.new(req.params))
+    elsif req.params.kind_of? Array then
+      return mod.send(op, *req.params)
+    else
+      return mod.send(op, req.params)
     end
 
-  end # handle()
-
+  end # handle_request()
 
   # Helper for creating JsonResponse
+  #
+  # @param [JsonRequest] req
+  #
+  # @return [JsonResponse]
   def unsupported_operation(req)
-      return render(:json => Bixby::JsonResponse.invalid_request("unsupported operation: '#{req.operation}'").to_json)
+    Bixby::JsonResponse.invalid_request("unsupported operation: '#{req.operation}'")
   end
 
-  # Extract JsonRequest
+  # Extract JsonRequest from the POST body
+  #
+  # @return [JsonRequest]
   def extract_request
 
     body = request.body.read.strip
