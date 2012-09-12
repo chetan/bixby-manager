ef114354c98730a31c052f339fad27afb0b603a6
Chetan Sarva <chetan@pixelcop.net>
2012-09-12 15:19:44 -0400

    implemented metadata search; allow search by tag using hash; added tests for search;

--- manager/app/models/host.rb
+++ manager/app/models/host.rb
@@ -29,52 +29,120 @@ class Host < ActiveRecord::Base
     return info
   end
 
+  def meta
+    return @meta if not @meta.nil?
+
+    @meta = {}
+    metadata.each do |m|
+      @meta[m.key] = m.value
+    end
+
+    return @meta
+  end
+
+  # Search for hosts matching the given terms
+  #
+  # Fields searched for keywords: hostname, ip, alias, desc
+  # Search by tag: tag:foo or #foo
+  # Search by metadata: foo=bar
+  #
+  # @param [String] terms
+  #
+  # @result [Array<Host>]
   def self.search(terms)
 
     keys = []
     tags = []
+    meta = {}
 
+    # parse terms
     terms.split(/\s+/).each do |s|
       if s =~ /^(\w+):(.*?)$/ then
-        key = $1
         val = $2
         if $1 =~ /^tags?$/ then
           tags += val.split(/,/)
         end
+
+      elsif s =~ /^#(.*?)$/ then
+        tags << $1
+
+      elsif s =~ /^(\w+)=(.*?)$/ then
+        meta[$1] = $2
+
       else
         keys << s
       end
     end
 
-    if not tags.blank? then
+    # find by tag
+    if not tags.empty? then
       hosts = Host.tagged_with(tags)
       if hosts.empty? then
         return []
       end
+
     else
-      hosts = Host.all # FIXME use keyword search?
+      hosts = Host.all # FIXME use keyword search? # restrict to ORG etc
+    end
+
+    # filter by metadata
+    if not meta.empty? then
+      found = []
+      hosts.each do |host|
+        all_keys_match = true
+        meta.keys.each do |key|
+          if host.meta[key] != meta[key] then
+            all_keys_match = false
+            break
+          end
+        end
+        found << host if all_keys_match
+      end
+
+      hosts = found
     end
 
     if keys.empty? then
       return hosts
     end
 
-    # further filter by keywords
+    # filter by keywords
     found = []
     hosts.each do |host|
       all_keys_match = true
       keys.each do |key|
-        if not (host.hostname.downcase.include? key or
-           host.alias.downcase.include? key or
-           host.desc.downcase.include? key) then
-
-            all_keys_match = false
+        if not test_key(host, [:hostname, :ip, :alias, :desc], key) then
+          all_keys_match = false
+          break
         end
       end
       found << host if all_keys_match
     end
     return found
 
+  end
+
+
+  private
+
+  # Test whether or not the keyword is present in one of the given fields
+  #
+  # @param [Host] host
+  # @param [Array<Symbol>] fields     names of fields to test
+  # @param [String] key               keyword
+  #
+  # @return [Boolean] true if keyword is present in at least one field
+  def self.test_key(host, fields, key)
+
+    match = false
+    fields.each do |f|
+      v = host.send(f)
+      if not v.blank? and v.include? key then
+        match = true
+      end
+    end
+
+    return match
   end
 
 end
--- manager/test/unit/models/host_test.rb
+++ manager/test/unit/models/host_test.rb
@@ -12,6 +12,21 @@ class Bixby::Test::Models::Host < ActiveSupport::TestCase
     DatabaseCleaner.clean
   end
 
+  def test_to_string
+    h = Host.new
+
+    h.ip = "4.4.4.4"
+    assert h.to_s
+    assert_equal "4.4.4.4", h.to_s
+
+
+    h.hostname = "foo.example.com"
+    assert_equal "foo.example.com", h.to_s
+
+    h.alias = "foo"
+    assert_equal "foo", h.to_s
+  end
+
   def test_info
     host = FactoryGirl.create(:host)
     host.metadata ||= []
@@ -20,5 +35,62 @@ class Bixby::Test::Models::Host < ActiveSupport::TestCase
 
     assert_equal 3, host.metadata.size
     assert_equal 1, host.info.size
+  end
+
+  def test_search_by_tag_and_kw
+    host = FactoryGirl.create(:host)
+    host.update_attributes({:tag_list => "foo, bar", :alias => "testing", :desc => "blarney stone"})
+
+    # tag only
+    h = Host.search("tag:foo").first
+    assert h
+    assert_equal host.id, h.id
+
+    h = Host.search("tags:foo").first
+    assert h
+    assert_equal host.id, h.id
+
+    h = Host.search("#foo").first
+    assert h
+    assert_equal host.id, h.id
+
+    # kw only
+    h = Host.search("testing").first
+    assert h
+    assert_equal host.id, h.id
+
+    # tag + kw
+    h = Host.search("testing tags:foo").first
+    assert h
+    assert_equal host.id, h.id
+
+    # multiple tags
+    h = Host.search("tag:bar tags:foo").first
+    assert h
+    assert_equal host.id, h.id
+
+    # multiple tags and kw
+    h = Host.search("stone tag:bar tags:foo").first
+    assert h
+    assert_equal host.id, h.id
+
+    # no results
+    assert_empty Host.search("asdf")
+    assert_empty Host.search("tag:baz")
+  end
+
+  def test_search_by_metadata
+    host = FactoryGirl.create(:host)
+    host.metadata ||= []
+    host.metadata += FactoryGirl.create_list(:metadata, 2)
+    host.metadata << Metadata.for("foo", "bar")
+
+    h = Host.search("foo=bar").first
+    assert h
+    assert_equal host.id, h.id
+
+    # no results
+    assert_empty Host.search("foo=baz")
+    assert_empty Host.search("bar=baz")
   end
 end
