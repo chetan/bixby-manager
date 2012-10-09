e00cfe8263748352e9c3dfd0ad0698a6313641bd
Chetan Sarva <chetan@pixelcop.net>
2012-10-09 15:38:00 -0400

    fixed asserts

--- test/unit/modules/scheduler_test.rb
+++ test/unit/modules/scheduler_test.rb
@@ -25,19 +25,19 @@ class Bixby::Test::Modules::Scheduler < ActiveSupport::TestCase
   def test_schedule_at
     Resque.reset_delayed_queue
     Bixby::Scheduler.new.schedule_at((Time.new+30), Bixby::Scheduler::Job.new("foobar", {}))
-    assert Resque.redis.zcard("delayed_queue_schedule") == 1 # key is namespaced
+    assert_equal 1, Resque.redis.zcard("delayed_queue_schedule") # key is namespaced
   end
 
   def test_schedule_in
     Resque.reset_delayed_queue
     Bixby::Scheduler.new.schedule_in(30, Bixby::Scheduler::Job.new("foobar", {}))
-    assert Resque.redis.zcard("delayed_queue_schedule") == 1 # key is namespaced
+    assert_equal 1, Resque.redis.zcard("delayed_queue_schedule") # key is namespaced
   end
 
   def test_schedule_in_with_queue
     Resque.reset_delayed_queue
     Bixby::Scheduler.new.schedule_in_with_queue(30, Bixby::Scheduler::Job.new("foobar", {}), "foo")
-    assert Resque.redis.zcard("delayed_queue_schedule") == 1 # key is namespaced
+    assert_equal 1, Resque.redis.zcard("delayed_queue_schedule") # key is namespaced
   end
 
   def test_driver_must_override_methods
