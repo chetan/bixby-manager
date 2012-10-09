a60ccc7d5c11e1a2124ece425bee3ff9bfc2a97d
Chetan Sarva <chetan@pixelcop.net>
2012-10-09 17:34:46 -0400

    added docs

--- lib/bixby/modules/scheduler/job.rb
+++ lib/bixby/modules/scheduler/job.rb
@@ -2,6 +2,14 @@
 module Bixby
 class Scheduler
 
+  # Describes a Job to be executed at a later time
+  #
+  # @!attribute [rw] klass
+  #   @return [Class] class which contains the target method
+  # @!attribute [rw] method
+  #   @return [Symbol] target method
+  # @!attribute [rw] args
+  #   @return [Array] list of arguments to pass to method call
   class Job
 
     attr_accessor :klass, :method, :args
@@ -12,12 +20,14 @@ class Scheduler
       @args = args
     end
 
+    # Called by Resque worker. Expects as input the output of #queue_args
     def self.perform(*args)
       klass = args.shift.constantize
       method = args.shift
       klass.new.send(method, *@args)
     end
 
+    # Returns arguments for use by Resque
     def queue_args
       [@klass.to_s, @method.to_s] + @args
     end
