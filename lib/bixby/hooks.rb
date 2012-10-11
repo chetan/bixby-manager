ccda410a85700549103caf1a273ebb5603006bc3
Chetan Sarva <chetan@pixelcop.net>
2012-10-11 15:53:31 -0400

    fixed before/after hook impl

--- lib/bixby/hooks.rb
+++ lib/bixby/hooks.rb
@@ -2,6 +2,42 @@
 module Bixby
 
   # Add before/after and named hooks (callbacks/events) to any class
+  #
+  # @example
+  #
+  #   class Foo
+  #     extend Bixby::Hooks
+  #     def hello(name)
+  #       puts "(orig) hello #{name}"
+  #     end
+  #   end
+  #
+  #   # standard call, no hooks
+  #   Foo.new.hello("joe")
+  #
+  #   # add before & after hooks
+  #   Foo.add_before_hook(:hello) do |name|
+  #     puts "(before) yo #{name}"
+  #   end
+  #
+  #   Foo.add_before_hook(:hello) do |name|
+  #     puts "(before) hulloooo #{name}"
+  #   end
+  #
+  #   Foo.add_after_hook(:hello) do |name|
+  #     puts "(after) sup #{name}"
+  #   end
+  #
+  #   Foo.new.hello("joe")
+  #
+  #   # outputs:
+  #
+  #   (orig) hello joe
+  #   (before) yo joe
+  #   (before) hulloooo joe
+  #   (orig) hello joe
+  #   (after) sup joe
+  #
   module Hooks
 
     # Add a named callback method. Either a Proc or Block must be given.
@@ -29,18 +65,13 @@ module Bixby
     # @param [Proc] proc        Proc to execute (optional)
     # @param [Block] block      Block to execute (optional)
     def add_before_hook(method, proc=nil, &block)
-      return if not methods.include? :method
+      return if not instance_methods.include? :method
       if not before_hooks.include? method then
         before_hooks[method] = []
       end
       hook = proc || block
       before_hooks[method] << hook
-      old_method = "#{method}_without_hooks"
-      alias_method old_method, method if not methods.include? old_method
-      send(:define_method, method) do |*args|
-        send(old_method, *args)
-        hook.call(*args)
-      end
+      add_around_hooks(method)
     end
 
     # Add a hook to run after the given method. Either a Proc or Block must be
@@ -50,18 +81,13 @@ module Bixby
     # @param [Proc] proc        Proc to execute (optional)
     # @param [Block] block      Block to execute (optional)
     def add_after_hook(method, proc=nil, &block)
-      return if not methods.include? :method
+      return if not instance_methods.include? :method
       if not after_hooks.include? method then
         after_hooks[method] = []
       end
       hook = proc || block
       after_hooks[method] << hook
-      old_method = "#{method}_without_hooks"
-      alias_method old_method, method if not methods.include? old_method
-      send(:define_method, method) do |*args|
-        send(old_method, *args)
-        hook.call(*args)
-      end
+      add_around_hooks(method)
     end
 
     # Get the list of registered hooks
@@ -96,6 +122,29 @@ module Bixby
       end
     end
     alias_method :run_hooks, :run_hook
+
+
+
+    private
+
+    # Setup before/after hook infrastructure
+    #
+    # @param [Symbol] method    Name of the method to hook
+    def add_around_hooks(method)
+      old_method = "#{method}_without_hooks"
+      return if instance_methods.include? old_method.to_sym
+
+      alias_method old_method, method
+      define_method(method) do |*args|
+        self.class.before_hooks[method].each do |hook|
+          hook.call(*args)
+        end
+        send(old_method, *args)
+        self.class.after_hooks[method].each do |hook|
+          hook.call(*args)
+        end
+      end
+    end
 
   end
 end
