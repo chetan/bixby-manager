e0c81b5f51a00bb7d51a81fb431111d1e4cc3d35
Chetan Sarva <chetan@pixelcop.net>
2012-10-25 20:16:59 -0400

    added docs; made more flexible

--- app/models/metric_info.rb
+++ app/models/metric_info.rb
@@ -3,8 +3,19 @@ class MetricInfo < ActiveRecord::Base
 
   belongs_to :command
 
+  # Retrieve the MetricInfo for the given Command
+  #
+  # @param [Command] command
+  # @param [String] key
+  #
+  # @return [MetricInfo]
   def self.for(command, key)
-    command = command.check.command if command.kind_of? Metric
+    if command.kind_of? Metric then
+      command = command.check.command
+      key = command.key
+    elsif command.kind_of? Check then
+      command = command.command
+    end
     where(:command_id => command.id, :metric => key)
   end
 
