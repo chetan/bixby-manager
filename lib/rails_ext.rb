eae2e9a1a9ed26a8b9859d0a683708f609668666
Chetan Sarva <chetan@pixelcop.net>
2012-10-31 16:37:13 -0400

    removing old to_api code

--- lib/rails_ext.rb
+++ lib/rails_ext.rb
@@ -1,7 +1,6 @@
 
 require 'rails_ext/csv_column'
 require 'rails_ext/json_column'
-# require 'rails_ext/to_api'
 require 'rails_ext/api_view/api_view'
 
 # these should only be used in non-production environments
