4f93db7d063796c840aa77cf8c4243862cf5c202
Chetan Sarva <chetan@pixelcop.net>
2012-11-01 01:01:06 -0400

    fixed includes

--- lib/rails_ext.rb
+++ lib/rails_ext.rb
@@ -1,6 +1,7 @@
 
 require 'rails_ext/csv_column'
 require 'rails_ext/json_column'
+require 'rails_ext/symbol_column'
 require 'rails_ext/api_view/api_view'
 
 # these should only be used in non-production environments
