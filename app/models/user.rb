12702a75bb229e3212701e11d43ad4a6c85cba09
Chetan Sarva <chetan@pixelcop.net>
2012-11-02 17:21:40 -0400

    added pw helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
 
   belongs_to :org
 
+  # Test the given password against the one on file
+  #
+  # @param [String] pw      plaintext password (unhashed)
+  #
+  # @return [Boolean] Returns true if passwords match
+  def test_password(pw)
+    SCrypt::Password.new(self.password) == pw
+  end
+
 end
