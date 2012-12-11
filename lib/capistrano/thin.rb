13f8171a59a817f7330a6dca6aef948734677ddc
Chetan Sarva <chetan@pixelcop.net>
2012-12-10 20:07:26 -0500

    don't load cap tasks during tests

--- lib/capistrano/thin.rb
+++ lib/capistrano/thin.rb
@@ -1,22 +1,26 @@
 
-namespace :thin do
-  %w(start stop restart).each do |action|
-    desc "#{action.capitalize} the Thin cluster"
-    task action.to_sym do
-      extra = (action == "restart" ? "-O -w 10" : "") # restart one-by-one with 10 sec wait
-      run  <<-CMD
-        cd /var/www/bixby/current; bundle exec thin #{action} #{extra} -C config/deploy/thin.yml
-      CMD
-    end
-  end
-end
+if Object.const_defined? :Capistrano then
 
-# "symlink" deploy:start/stop/restart to thin:start/stop/restart
-namespace :deploy do
-  %w(start stop restart).each do |action|
-     desc "#{action.capitalize} the Thin cluster"
-     task action.to_sym do
-       find_and_execute_task("thin:#{action}")
+  namespace :thin do
+    %w(start stop restart).each do |action|
+      desc "#{action.capitalize} the Thin cluster"
+      task action.to_sym do
+        extra = (action == "restart" ? "-O -w 10" : "") # restart one-by-one with 10 sec wait
+        run  <<-CMD
+          cd /var/www/bixby/current; bundle exec thin #{action} #{extra} -C config/deploy/thin.yml
+        CMD
+      end
     end
   end
+
+  # "symlink" deploy:start/stop/restart to thin:start/stop/restart
+  namespace :deploy do
+    %w(start stop restart).each do |action|
+       desc "#{action.capitalize} the Thin cluster"
+       task action.to_sym do
+         find_and_execute_task("thin:#{action}")
+      end
+    end
+  end
+
 end
--- lib/capistrano/uname.rb
+++ lib/capistrano/uname.rb
@@ -1,7 +1,11 @@
 
-namespace :misc do
-  desc "get uname for each server"
-  task :uname do
-    run "uname -a"
+if Object.const_defined? :Capistrano then
+
+  namespace :misc do
+    desc "get uname for each server"
+    task :uname do
+      run "uname -a"
+    end
   end
+
 end
