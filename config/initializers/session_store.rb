3b84c200272140a7aa468ec49d2842e1451c7beb
Chetan Sarva <chetan@pixelcop.net>
2012-12-26 16:03:18 -0500

    use db for session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,2 @@
-# Be sure to restart your server when you modify this file.
 
-Bixby::Application.config.session_store :cookie_store, :key => '_bixby_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Bixby::Application.config.session_store :active_record_store
+Bixby::Application.config.session_store :active_record_store
