5d438aff8b3baf199da48bfa3b308382e43fb3e2
Chetan Sarva <chetan@pixelcop.net>
2013-01-26 01:12:56 -0500

    check body after authenticating

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -103,18 +103,19 @@ class ApiController < ApplicationController
   # @return [JsonRequest]
   def extract_request
 
-    body = request.body.read.strip
-    if body.blank? then
-      return Bixby::JsonResponse.invalid_request
-    end
-
-    # decrypt the body if necessary
+    # authenticate the request
     if crypto_enabled? then
       @agent = Agent.where(:access_key => ApiAuth.access_id(request)).first
       if not (@agent and ApiAuth.authentic?(request, @agent.secret_key)) then
         return Bixby::JsonResponse.new("fail", "authentication failed", nil, 401)
       end
       MultiTenant.current_tenant = @agent.tenant
+    end
+
+    request.body.rewind if request.body.respond_to?(:rewind)
+    body = request.body.read.strip
+    if body.blank? then
+      return Bixby::JsonResponse.invalid_request
     end
 
     begin
