8be99106a1ec9f55385a3614cf3c63350fafe65a
Chetan Sarva <chetan@pixelcop.net>
2013-02-09 00:56:22 -0500

    fix path issue

--- config/deploy/rvm_wrapper.sh
+++ config/deploy/rvm_wrapper.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-source $rvm_path/environments/$USE_RUBY_VERSION
+# TODO fix path
+#
+# it seems when god drops privileges via Process::Sys.setuid/gid
+# we do not inherit the proper environment. therefore we must be very
+# careful and use workarounds and/or absolute paths
+#
+# i.e., in this case, $rvm_path would would inherit from root
+
+source /home/`whoami`/.rvm/environments/$USE_RUBY_VERSION
 $*
