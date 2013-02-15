e509b8f42c0ade0b36e1788e333eb1a5f5ae38b6
Chetan Sarva <chetan@pixelcop.net>
2013-02-14 21:58:24 -0500

    moved deploy vars to bixby.yml

--- config/deploy/bixby.god
+++ config/deploy/bixby.god
@@ -1,15 +1,18 @@
 
 # TODO move vars to config file
 
-ENV["USE_RUBY_VERSION"] = "ruby-1.9.3-p385"
-
-USER  = "chetan"
-GROUP = "chetan"
-
 RAILS_ENV    = ENV['RAILS_ENV']  = 'production'
 RAILS_ROOT   = ENV['RAILS_ROOT'] = '/var/www/bixby/current'
 
 RVM_WRAPPER  = File.join(RAILS_ROOT, "config", "deploy", "rvm_wrapper.sh")
+
+# gets passed to RVM_WRAPPER SCRIPT
+conf = YAML.load_file(File.join(RAILS_ROOT, "config", "bixby.yml"))[RAILS_ENV]
+ENV["USE_RUBY_VERSION"] = conf["ruby"]
+ENV["USE_RVM"]          = conf["rvm"]
+
+USER  = conf["user"]
+GROUP = conf["group"]
 
 %w{unicorn sidekiq}.each do |file|
   God.load File.join(RAILS_ROOT, "config", "deploy", "god", "#{file}.god")
--- config/deploy/rvm_wrapper.sh
+++ config/deploy/rvm_wrapper.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-# TODO fix path
-#
 # it seems when god drops privileges via Process::Sys.setuid/gid
 # we do not inherit the proper environment. therefore we must be very
-# careful and use workarounds and/or absolute paths
+# careful and use workarounds and/or absolute paths.
 #
-# i.e., in this case, $rvm_path would would inherit from root
+# i.e., in this case, $rvm_path would would inherit from root and we want
+#       to be very explicit about which ruby to use.
 
-source /home/`whoami`/.rvm/environments/$USE_RUBY_VERSION
+if [[ $USE_RVM == "system" ]]; then
+  source /usr/local/rvm/environments/$USE_RUBY_VERSION
+else
+  source /home/`whoami`/.rvm/environments/$USE_RUBY_VERSION
+fi
+
 $*
