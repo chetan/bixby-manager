64e2f76381d30f51cd30214fcf411e8c13005385
Chetan Sarva <chetan@pixelcop.net>
2013-03-15 18:24:59 -0400

    display bixby version as meta tag

--- lib/bixby.rb
+++ lib/bixby.rb
@@ -3,6 +3,18 @@ module Bixby
   require "bixby/file_download"
   require "bixby/hooks"
   require "bixby/async"
+
+  class << self
+    attr_accessor :ref
+  end
+end
+
+# set version
+rev_file = File.join(Rails.root, "REVISION")
+if File.exists? rev_file then
+  Bixby.ref = File.read(rev_file).strip
+else
+  Bixby.ref = "HEAD"
 end
 
 require 'bixby/modules'
