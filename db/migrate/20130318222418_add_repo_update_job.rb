663bde140fe6f7a11fb3b3660d0b04928411bd7c
Chetan Sarva <chetan@pixelcop.net>
2013-03-18 18:55:07 -0400

    fixed namespace

--- db/migrate/20130318222418_add_repo_update_job.rb
+++ db/migrate/20130318222418_add_repo_update_job.rb
@@ -1,6 +1,6 @@
 class AddRepoUpdateJob < ActiveRecord::Migration
   def up
-    job = RecurringJob.create(1.hour, Bixby::Repository, :update)
+    job = Bixby::Scheduler::RecurringJob.create(1.hour, Bixby::Repository, :update)
     Bixby::Scheduler.new.schedule_in(1.hour, job)
   end
 
