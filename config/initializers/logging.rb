12d72a3c647f3151ff86dddad3db356fb6fb06d5
Chetan Sarva <chetan@pixelcop.net>
2013-04-06 16:13:39 -0400

    use a custom appender for capturing logs during tests

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,3 +1,37 @@
 
 # lower to info (always)
 ::Rails.cache.logger.level = :info
+
+# Use a custom logger for the Bixby::* namespace when in TEST env
+if ::Rails.env == "test" then
+
+  # A simple subclass of the Stdout appender which writes to whatever
+  # $stdout is currently pointing to
+  module Logging::Appenders
+    class StdoutTest < Stdout
+      def canonical_write( str )
+        return self if @io.nil?
+        str = str.force_encoding(encoding) if encoding and str.encoding != encoding
+        $stdout.syswrite str
+        self
+      rescue StandardError => err
+        self.level = :off
+        ::Logging.log_internal {"appender #{name.inspect} has been disabled"}
+        ::Logging.log_internal(-2) {err}
+      end
+    end
+  end
+
+  Logging::Appenders::StdoutTest.new( 'stdout_test',
+    :auto_flushing => true,
+    :layout => Logging.layouts.pattern(
+      :pattern => '%.1l, [%d] %5l -- %c: %m\n',
+      :color_scheme => 'bright'
+    )
+  )
+
+  bixby = Logging.logger[Bixby]
+  bixby.clear_appenders
+  bixby.add_appenders("stdout_test")
+
+end
