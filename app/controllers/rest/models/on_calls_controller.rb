178ef4062450a085e4dbd4eb5e6a2d11ba70e309
Chetan Sarva <chetan@pixelcop.net>
2013-05-03 16:02:47 -0400

    cleanup

--- app/controllers/rest/models/on_calls_controller.rb
+++ app/controllers/rest/models/on_calls_controller.rb
@@ -7,31 +7,14 @@ class Rest::Models::OnCallsController < UiController
   end
 
   def show
-    @host = Host.find(params[:id])
+    oncall = OnCall.find(_id)
     restful @host
   end
 
   def update
-    @host = Host.find(params[:id])
-    attrs = pick(:alias, :desc)
-    attrs[:tag_list] = params[:tags]
-    @host.update_attributes(attrs)
-
-    restful @host
   end
 
   def destroy
-    @host = Host.find(params[:id])
-    @host.destroy
-    @host.agent.destroy if @host.agent
-
-    restful @host
-  end
-
-  def update_facts
-    @host = Host.find(params[:id])
-    Bixby::Inventory.new.update_facts(@host)
-    restful @host
   end
 
 end
