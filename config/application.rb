fc34a78d522629af06381b47ff422a8cf4dd984b
Chetan Sarva <chetan@pixelcop.net>
2013-06-28 15:41:12 -0400

    dont connect to db during precompile

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
@@ -66,6 +66,7 @@ module Bixby
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # disable Rack::Cache middleware
     config.action_dispatch.rack_cache = nil
