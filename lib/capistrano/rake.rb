13e63c225688a3983983df283be1cb5eba43ff1d
Chetan Sarva <chetan@pixelcop.net>
2013-07-23 12:45:30 -0400

    fixed rake namespace clash

--- lib/capistrano/rake.rb
+++ lib/capistrano/rake.rb
@@ -1,8 +1,9 @@
 
-namespace :rake do
+# namespace is raketask due to conflicts with :rake var
+namespace :raketask do
   desc "Run a task on a remote server."
   # run like: cap staging rake:invoke task=a_certain_task
   task :invoke do
-    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['task']} RAILS_ENV=#{rails_env}")
+    run("cd #{deploy_to}/current; #{rake} #{ENV['task']} RAILS_ENV=#{rails_env}")
   end
 end
