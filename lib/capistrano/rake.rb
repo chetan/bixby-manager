0ef5e5f4fd87660515286fadd91e09f63e2cfca3
Chetan Sarva <chetan@pixelcop.net>
2013-08-02 12:26:19 -0400

    updated task docs

--- lib/capistrano/rake.rb
+++ lib/capistrano/rake.rb
@@ -1,8 +1,8 @@
 
 # namespace is raketask due to conflicts with :rake var
 namespace :raketask do
-  desc "Run a task on a remote server."
-  # run like: cap staging rake:invoke task=a_certain_task
+  desc "Run a task on a remote server (usage: raketask:invoke task=mytask)"
+  # run like: cap staging raketask:invoke task=a_certain_task
   task :invoke do
     run("cd #{deploy_to}/current; #{rake} #{ENV['task']} RAILS_ENV=#{rails_env}")
   end
