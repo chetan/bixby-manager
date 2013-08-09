1883a22ad75d01cc140a40c34d3a197a201cc579
Chetan Sarva <chetan@pixelcop.net>
2013-08-09 19:16:23 -0400

    fixed exception on back/forward

--- app/controllers/rest/models/metrics_controller.rb
+++ app/controllers/rest/models/metrics_controller.rb
@@ -13,7 +13,7 @@ class Rest::Models::MetricsController < UiController
   def show
     metric = Metric.find(_id)
     downsample = params[:downsample] || "5m-avg"
-    metric.load_data!(_id(:start), _id(:end), {}, "sum", downsample)
+    metric.load_data!(_id(:start, true), _id(:end, true), {}, "sum", downsample)
 
     restful metric
   end
