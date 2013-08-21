33a7aa9ced308df9cecc195665d3b5fa4b8e5c65
Chetan Sarva <chetan@pixelcop.net>
2013-08-21 12:20:32 -0400

    move common websocket code to bixby-common

--- lib/bixby/websocket.rb
+++ lib/bixby/websocket.rb
@@ -1,10 +1,2 @@
 
-module Bixby
-  module WebSocket
-  end
-end
-
-require "bixby/websocket/api"
-require "bixby/websocket/async_request"
 require "bixby/websocket/server"
-require "bixby/websocket/client"
