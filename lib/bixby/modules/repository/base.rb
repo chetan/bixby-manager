16c6ec42667774e2330e6fb26a054587fbda13a8
Chetan Sarva <chetan@pixelcop.net>
2013-08-27 15:00:54 -0400

    don't extend api; just include Log

--- lib/bixby/modules/repository/base.rb
+++ lib/bixby/modules/repository/base.rb
@@ -2,14 +2,20 @@
 module Bixby
 class Repository < API
 
-  class BaseRepo < API
+  class BaseRepo
+
+    include Bixby::Log
+
     attr_accessor :repo
+
     def initialize(repo)
       @repo = repo
     end
+
     def clone
       raise NotImplementedError
     end
+
     def update
       raise NotImplementedError
     end
