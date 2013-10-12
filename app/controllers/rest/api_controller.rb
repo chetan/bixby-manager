86aca380bdbc4a6c63a5f5ad03ed0d96f758b859
Chetan Sarva <chetan@pixelcop.net>
2013-10-11 20:10:11 -0400

    check both header keys

--- app/controllers/rest/api_controller.rb
+++ app/controllers/rest/api_controller.rb
@@ -10,7 +10,7 @@ class Rest::ApiController < UiController
   # @return [Boolean] true if the request is not authenticated (user must login)
   def login_required?
     # check for API/signed requests
-    if request.headers.env["Authorization"] then
+    if request.headers.env["HTTP_AUTHORIZATION"] || request.headers.env["Authorization"] then
       agent = Agent.where(:access_key => ApiAuth.access_id(request)).first
       if not(agent and ApiAuth.authentic?(request, agent.secret_key)) then
         return render :text => Bixby::JsonResponse.new("fail", "authentication failed", nil, 401).to_json, :status => 401
