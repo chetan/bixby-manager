ec7c1f69fc31096d86a50b07de2364899d9443d0
Chetan Sarva <chetan@pixelcop.net>
2013-10-30 10:49:39 -0400

    use init script for restart

--- lib/capistrano/bixby.rb
+++ lib/capistrano/bixby.rb
@@ -5,7 +5,7 @@ if Object.const_defined? :Capistrano then
     %w(start stop restart).each do |action|
       desc "#{action.capitalize} the Bixby services"
       task action.to_sym do
-        run "#{sudo} god #{action} bixby"
+        run "#{sudo} /etc/init.d/bixby-server god #{action} bixby"
       end
     end
   end
