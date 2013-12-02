f71e06bfe4e0a8209dc7d58b45482c6d1fe6ff04
Chetan Sarva <chetan@pixelcop.net>
2013-12-02 16:03:26 -0500

    don't downsample new metrics

--- app/controllers/monitoring/metrics_controller.rb
+++ app/controllers/monitoring/metrics_controller.rb
@@ -3,7 +3,13 @@ class Monitoring::MetricsController < Monitoring::BaseController
 
   def show
     metric = Metric.find(_id)
-    downsample = params[:downsample] || "5m-avg"
+
+    downsample = params[:downsample]
+    if downsample.blank? then
+      # down't downsample when less than 12hrs of data avail
+      downsample = (Time.new - metric.created_at < 43200) ? nil : "5m-avg"
+    end
+
     metric.load_data!(params[:start], params[:end], {}, "sum", downsample)
 
     bootstrap metric, metric.check, metric.check.host
