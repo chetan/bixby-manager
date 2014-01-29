c2deef4779c87d82b0bee81e62af31a9d6b4b32a
Chetan Sarva <chetan@pixelcop.net>
2014-01-29 13:43:16 -0500

    added mailer config for devise

--- config/environments/integration.rb
+++ config/environments/integration.rb
@@ -57,6 +57,9 @@ Bixby::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  # for devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
