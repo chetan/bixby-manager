a1bedc032ab6e17c966257e232926995bf26e95a
Chetan Sarva <chetan@pixelcop.net>
2014-02-07 13:54:05 -0500

    updated taggable gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128170149) do
+ActiveRecord::Schema.define(version: 20140207175214) do
 
   create_table "actions", force: true do |t|
     t.integer  "trigger_id",            null: false
@@ -247,12 +247,14 @@ ActiveRecord::Schema.define(version: 20140128170149) do
     t.datetime "created_at"
   end
 
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
   add_index "taggings", ["tag_id"], name: "fk_taggings_tags1", using: :btree
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: true do |t|
     t.string "name"
   end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "tenants", force: true do |t|
     t.string "name"
