74880c8fc40a05d720aea7dfc356c6e2dbb5ae98
Chetan Sarva <chetan@pixelcop.net>
2014-02-14 14:29:50 -0500

    [refactor] moved test_metrics to hooks module

--- lib/bixby/modules/monitoring.rb
+++ lib/bixby/modules/monitoring.rb
@@ -6,10 +6,6 @@ class Monitoring < API
   GET_OPTIONS = "--options"
   GET_METRICS = "--monitor"
 
-  Bixby::Metrics.add_hook(:put_check_result) do |metrics|
-    Monitoring.new.test_metrics(metrics)
-  end
-
   # Get command options specific to the specified agent
   #
   # @param [Agent] agent
@@ -150,85 +146,6 @@ class Monitoring < API
     return check
   end
 
-  # Test the given list of metrics for triggers
-  #
-  # @param [Array<Metric>] metrics
-  def test_metrics(metrics)
-
-    all_triggers = get_all_triggers(metrics)
-    metrics.each do |metric|
-
-      triggers = all_triggers.find_all { |a|
-        a.metric_id == metric.id or a.check_id == metric.check_id
-      }
-      next if triggers.blank?
-
-      triggered = []
-      reset = []
-
-      triggers.each do |trigger|
-        logger.debug { "testing #{metric.key}: #{metric.last_value} #{trigger.sign} #{trigger.threshold.to_s}" }
-        if trigger.test_value(metric.last_value) or trigger.test_status(metric.last_status) then
-          # trigger is over threshold
-          if trigger.severity == metric.status then
-            next # already in this state, skip
-          end
-          logger.debug { "#{metric.key}: triggered" }
-          triggered << trigger
-
-        elsif metric.status != Metric::Status::OK then
-          # trigger has returned to normal
-          logger.debug { "#{metric.key}: reset" }
-          reset << trigger
-        end
-      end # triggers.each
-
-      # process triggers over threshold
-      filter_triggers(triggered).each do |trigger|
-        # store history
-        history = TriggerHistory.record(metric, trigger)
-        metric.status = trigger.severity
-        metric.save!
-
-        # process all actions
-        trigger.actions.each do |action|
-          if action.alert? then
-            # notify
-            oncall = OnCall.find(action.target_id)
-            MonitoringMailer.alert(metric, trigger, oncall.current_user).deliver
-
-          elsif action.exec? then
-            # run command
-            cmd = Command.find(action.target_id)
-            # TODO run it
-          end
-        end
-      end # triggered
-
-      # only proceed if all triggers did not match
-      next if not triggered.blank?
-
-      # metric is back to normal level
-      filter_triggers(reset).each do |trigger|
-        metric.status = Metric::Status::OK
-        metric.save!
-        previous_history = TriggerHistory.previous_for_trigger(trigger)
-        history = TriggerHistory.record(metric, trigger)
-
-        # process all actions
-        trigger.actions.each do |action|
-          if action.alert? then
-            # notify
-            oncall = OnCall.find(action.target_id)
-            MonitoringMailer.alert(metric, trigger, oncall.current_user).deliver
-          end
-          # we ignore exec actions for now
-        end
-      end # reset
-
-    end # metrics.each
-  end # test_metrics()
-
   # Create a new trigger
   #
   # @param [Hash] opts
@@ -276,38 +193,6 @@ class Monitoring < API
 
 
   private
-
-  # Filter the given list of triggers
-  # * If any are CRITICAL, returns only those
-  # * Otherwise returns all (no filtering done)
-  #
-  # @param [Array<Trigger>] triggers
-  #
-  # @return [Array<Trigger>] filtered list of triggers
-  def filter_triggers(triggers)
-    if triggers.size > 1 then
-      # get only CRITICAL triggers
-      filtered = triggers.find_all { |t| t.severity == Trigger::Severity::CRITICAL }
-    end
-    if filtered.blank? then
-      filtered = triggers
-    end
-    return filtered
-  end
-
-  # Get all triggers matching the list of metrics (in a single query)
-  def get_all_triggers(metrics)
-    metric_ids = []
-    check_ids = []
-    metrics.each do |m|
-      metric_ids << m.id
-      check_ids << m.check_id
-    end
-
-    return Trigger.where("metric_id IN (?) OR check_id IN (?)",
-                         metric_ids,
-                         check_ids.sort.uniq)
-  end
 
   # Create a CommandSpec for the given Check
   #
--- lib/bixby/modules/monitoring/hooks.rb
+++ lib/bixby/modules/monitoring/hooks.rb
@@ -6,9 +6,92 @@ class Monitoring < API
 
     extend Bixby::Log
 
+    Bixby::Metrics.add_hook(:put_check_result) do |metrics|
+      test_metrics(metrics)
+    end
+
     Bixby::Inventory.add_hook(:register_agent) do |agent|
       add_checks_on_register(agent)
     end
+
+    # Test the given list of metrics for triggers
+    #
+    # @param [Array<Metric>] metrics
+    def self.test_metrics(metrics)
+
+      all_triggers = get_all_triggers(metrics)
+      metrics.each do |metric|
+
+        triggers = all_triggers.find_all { |a|
+          a.metric_id == metric.id or a.check_id == metric.check_id
+        }
+        next if triggers.blank?
+
+        triggered = []
+        reset = []
+
+        triggers.each do |trigger|
+          logger.debug { "testing #{metric.key}: #{metric.last_value} #{trigger.sign} #{trigger.threshold.to_s}" }
+          if trigger.test_value(metric.last_value) or trigger.test_status(metric.last_status) then
+            # trigger is over threshold
+            if trigger.severity == metric.status then
+              next # already in this state, skip
+            end
+            logger.debug { "#{metric.key}: triggered" }
+            triggered << trigger
+
+          elsif metric.status != Metric::Status::OK then
+            # trigger has returned to normal
+            logger.debug { "#{metric.key}: reset" }
+            reset << trigger
+          end
+        end # triggers.each
+
+        # process triggers over threshold
+        filter_triggers(triggered).each do |trigger|
+          # store history
+          history = TriggerHistory.record(metric, trigger)
+          metric.status = trigger.severity
+          metric.save!
+
+          # process all actions
+          trigger.actions.each do |action|
+            if action.alert? then
+              # notify
+              oncall = OnCall.find(action.target_id)
+              MonitoringMailer.alert(metric, trigger, oncall.current_user).deliver
+
+            elsif action.exec? then
+              # run command
+              cmd = Command.find(action.target_id)
+              # TODO run it
+            end
+          end
+        end # triggered
+
+        # only proceed if all triggers did not match
+        next if not triggered.blank?
+
+        # metric is back to normal level
+        filter_triggers(reset).each do |trigger|
+          metric.status = Metric::Status::OK
+          metric.save!
+          previous_history = TriggerHistory.previous_for_trigger(trigger)
+          history = TriggerHistory.record(metric, trigger)
+
+          # process all actions
+          trigger.actions.each do |action|
+            if action.alert? then
+              # notify
+              oncall = OnCall.find(action.target_id)
+              MonitoringMailer.alert(metric, trigger, oncall.current_user).deliver
+            end
+            # we ignore exec actions for now
+          end
+        end # reset
+
+      end # metrics.each
+    end # test_metrics()
 
     # Apply Check Template rules to the newly registered agent
     #
@@ -52,9 +135,45 @@ class Monitoring < API
 
       end
 
+    end # self.add_checks_on_register
+
+    private
+
+
+    # Filter the given list of triggers
+    # * If any are CRITICAL, returns only those
+    # * Otherwise returns all (no filtering done)
+    #
+    # @param [Array<Trigger>] triggers
+    #
+    # @return [Array<Trigger>] filtered list of triggers
+    def self.filter_triggers(triggers)
+      if triggers.size > 1 then
+        # get only CRITICAL triggers
+        filtered = triggers.find_all { |t| t.severity == Trigger::Severity::CRITICAL }
+      end
+      if filtered.blank? then
+        filtered = triggers
+      end
+      return filtered
     end
 
-  end
+    # Get all triggers matching the list of metrics (in a single query)
+    def self.get_all_triggers(metrics)
+      metric_ids = []
+      check_ids = []
+      metrics.each do |m|
+        metric_ids << m.id
+        check_ids << m.check_id
+      end
+
+      return Trigger.where("metric_id IN (?) OR check_id IN (?)",
+                           metric_ids,
+                           check_ids.sort.uniq)
+    end
+
+
+  end # Hooks
 
 end
 end
