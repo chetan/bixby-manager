70dc1730b248371d40be743f031381a1e8028c8d
Chetan Sarva <chetan@pixelcop.net>
2014-03-26 12:59:16 -0400

    namespace task

--- lib/capistrano/rake.rb
+++ lib/capistrano/rake.rb
@@ -1,9 +1,13 @@
 
-# namespace is raketask due to conflicts with :rake var
-namespace :raketask do
-  desc "Run a task on a remote server (usage: raketask:invoke task=mytask)"
-  # run like: cap staging raketask:invoke task=a_certain_task
-  task :invoke do
-    run("cd #{deploy_to}/current; #{rake} #{ENV['task']} RAILS_ENV=#{rails_env}")
+if Object.const_defined? :Capistrano then
+
+  # namespace is raketask due to conflicts with :rake var
+  namespace :raketask do
+    desc "Run a task on a remote server (usage: raketask:invoke task=mytask)"
+    # run like: cap staging raketask:invoke task=a_certain_task
+    task :invoke do
+      run("cd #{deploy_to}/current; #{rake} #{ENV['task']} RAILS_ENV=#{rails_env}")
+    end
   end
+
 end
