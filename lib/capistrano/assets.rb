d0d2bcccca69a44fe8625bdef76238cb5c1294fd
Chetan Sarva <chetan@pixelcop.net>
2014-03-28 17:12:10 -0400

    fix typo

--- lib/capistrano/assets.rb
+++ lib/capistrano/assets.rb
@@ -9,7 +9,7 @@ if Object.const_defined? :Capistrano then
     namespace :assets do
       task :precompile, :roles => :web, :except => { :no_release => true } do
         from = source.next_revision(current_revision)
-        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/asaets/ app/assets/ lib/assets | wc -l").to_i > 0
+        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets | wc -l").to_i > 0
           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
         else
           logger.info "Skipping asset pre-compilation because there were no asset changes"
