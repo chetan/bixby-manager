0b51874c94ea8ee098ee31b87447d557546dfb9a
Chetan Sarva <chetan@pixelcop.net>
2014-07-04 12:19:43 -0400

    quiet sidekiq logs in dev

--- lib/ext/sidekiq_logging.rb
+++ lib/ext/sidekiq_logging.rb
@@ -4,6 +4,11 @@ if Module.const_defined? :Sidekiq then
 
   Sidekiq::Logging.logger = Logging.logger[Sidekiq]
 
+  if Rails.env == "development" then
+    # quiet sidekiq logs in dev mode
+    Logging.logger.root.level = :info
+  end
+
   module Sidekiq
     module Logging
       def self.with_context(msg)
