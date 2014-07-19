14943d8d12dfbfd2913a508e82e6d6f7e1410237
Chetan Sarva <chetan@pixelcop.net>
2014-07-19 11:19:36 -0400

    don't include console_table in test env

--- lib/rails_ext.rb
+++ lib/rails_ext.rb
@@ -6,7 +6,9 @@ require 'rails_ext/json_column'
 require 'rails_ext/symbol_column'
 require 'rails_ext/api_view/api_view'
 
-begin
-  require 'rails_ext/console_table'
-rescue LoadError
+if ! Rails.env.test? then
+  begin
+    require 'rails_ext/console_table'
+  rescue LoadError
+  end
 end
