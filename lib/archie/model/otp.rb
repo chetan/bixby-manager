96a282aa7f41bdbcbb8d24367bf9ac47dadc69b6
Chetan Sarva <chetan@pixelcop.net>
2014-08-01 20:47:16 -0400

    added OTP validation routines

--- lib/archie/model/otp.rb
+++ lib/archie/model/otp.rb
@@ -13,6 +13,26 @@ module Archie
         attr_accessor :otp_attempt
       end
 
+      # Validate the given OTP code
+      def valid_otp?(code)
+        return false unless otp_secret.present?
+
+        totp = self.otp(otp_secret)
+        totp.verify_with_drift(code, Archie::Config.otp_allowed_drift)
+      end
+
+      def otp(otp_secret = self.otp_secret)
+        ROTP::TOTP.new(otp_secret)
+      end
+
+      def current_otp
+        otp.at(Time.now)
+      end
+
+      def otp_provisioning_uri(account, options = {})
+        ROTP::TOTP.new(otp_secret, options).provisioning_uri(account)
+      end
+
       module ClassMethods
         def generate_otp_secret(otp_secret_length = Archie::Config.otp_secret_length)
           ROTP::Base32.random_base32(otp_secret_length)
