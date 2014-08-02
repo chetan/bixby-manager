f7c6200ac3418a1f6fcdd17ae09583532a3cc73f
Chetan Sarva <chetan@pixelcop.net>
2014-08-01 20:46:31 -0400

    fixed password validation

--- lib/archie/model.rb
+++ lib/archie/model.rb
@@ -10,9 +10,9 @@ module Archie
       attr_reader :password, :current_password
       attr_accessor :password_confirmation
 
-      validates_presence_of     :password
-      validates_confirmation_of :password
-      validates_length_of       :password, :minimum => 8
+      validates_presence_of     :password, :if => :password_required?
+      validates_confirmation_of :password, :if => :password_required?
+      validates_length_of       :password, :minimum => 8, :if => :password_required?
     end
 
     def valid_password?(pass)
@@ -36,6 +36,13 @@ module Archie
 
 
     private
+
+    # Checks whether a password is needed or not. For validations only.
+    # Passwords are always required if it's a new record, or if the password
+    # or confirmation are being set somewhere.
+    def password_required?
+      !persisted? || !password.nil? || !password_confirmation.nil?
+    end
 
     def password_digest(password)
       SCrypt::Password.create("#{@password}#{password_salt}#{Archie::Config.pepper}").to_s
