730589831e96ae8d17760816e25f266e38e3416c
Chetan Sarva <chetan@pixelcop.net>
2014-08-12 14:09:59 -0400

    update ranges on migration

--- db/migrate/20140812133656_add_range_to_metrics.rb
+++ db/migrate/20140812133656_add_range_to_metrics.rb
@@ -1,5 +1,7 @@
 class AddRangeToMetrics < ActiveRecord::Migration
   def change
     add_column :metrics, :range, :string, :length => 255, :after => :key
+    Metric.reset_column_information
+    Bixby::Monitoring.new.update_metric_ranges
   end
 end
