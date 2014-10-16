c0c469f891dc89b583d71a4f8d33582a15b323ea
Chetan Sarva <chetan@pixelcop.net>
2014-10-16 14:42:51 -0400

    PumaRunner: recv_io(TCPServer)
    * instead of calling TCPServer.from_fd(io.to_i) which was causing us
      lots of problems with "bad file descriptor" errors and the server
      dying after a short while.

--- lib/puma_runner/socket_binder.rb
+++ lib/puma_runner/socket_binder.rb
@@ -15,14 +15,14 @@ module PumaRunner
 
         # got a socket url, read io
         url = str
-        fd = conn.recv_io
+        fd = conn.recv_io(TCPServer)
 
         # try to validate it first
         if !fd.kind_of?(IO) || fd.closed? then
           raise SocketError, "received an invalid socket fd"
         end
 
-        @inherited_fds[url] = fd.to_i
+        @inherited_fds[url] = fd
       end
     end
 
