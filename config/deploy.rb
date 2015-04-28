95382d9774bdedfa8afd6ea82ba7ab7f935c0087
Chetan Sarva <chetan@pixelcop.net>
2015-04-28 14:47:22 -0400

    [deploy] restart after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,7 @@ after "deploy:restart", "deploy:cleanup"
 set :conditionally_migrate, false
 # after 'deploy:update_code', 'deploy:migrate'
 
+after "deploy:published", "bixby:restart"
 
 # namespace :deploy do
 
