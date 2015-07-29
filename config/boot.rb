55ba7dfd21ac04b9730687ae24bf950c22f08a77
Chetan Sarva <chetan@pixelcop.net>
2015-07-29 14:17:30 -0400

    [puma] always bind to 127.0.0.1
    * 'localhost' will resolve to ::1 on osx which breaks some clients

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,12 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host: '127.0.0.1', Port: 3000)
+    end
+  end
+end
