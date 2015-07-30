f5533af4197b36e058f19977f7f4ef2b4ae2cab6
Chetan Sarva <chetan@pixelcop.net>
2015-07-30 15:07:41 -0400

    [gem] upgraded to latest Puma

--- config.ru
+++ config.ru
@@ -5,7 +5,7 @@ ENV["IS_RAILS_SERVER"] = "1"
 require ::File.expand_path('../config/environment',  __FILE__)
 
 wsapp = Rack::Builder.new do
-  use Rack::CommonLogger, Rails.logger
+  use Puma::CommonLogger, Rails.logger
   run Bixby::WebSocketServer.new
 end
 
--- lib/rails_ext/rack_hijack.rb
+++ lib/rails_ext/rack_hijack.rb
@@ -1,13 +1,18 @@
 
-require 'rack/commonlogger'
-require 'puma/rack_patch'
+# Instead of logging a standard access_log line, write a proper log of the connection
+#
+# e.g., instead of this:
+# 127.0.0.1 - - [30/Jul/2015:10:46:11 -0400] "GET /wsapi HTTP/1.1" -1 - 0.0640
+# write this:
+# I, [2015-07-30 15:06:12]  INFO -- Puma::CommonLogger:23: new websocket connection from 127.0.0.1
 
-module Rack
-  # Patch CommonLogger to use after_reply.
-  #
-  # Simply request this file and CommonLogger will be a bit more
-  # efficient.
+require "puma/commonlogger"
+
+module Puma
   class CommonLogger
+
+    private
+
     def log_hijacking(env, status, header, began_at)
       ::Logging::Logger[self].info {
         ip = env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-"
